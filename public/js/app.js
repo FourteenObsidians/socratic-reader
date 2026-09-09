fbc7e9e2269d9a635e49bd79133f4380e63eca27
FourteenObsidians <54407051+FourteenObsidians@users.noreply.github.com>
2026-09-09 10:56:16 +0800

    fix v40 带读去元叙述：论文/教材主语句每轮至多1句，改掉提示词里的示范句式

--- public/js/app.js
+++ public/js/app.js
@@ -251,7 +251,7 @@ SR.zotero = {
 
 /* ---------- 启动 ---------- */
 (async function init() {
-  SR.VERSION = 'v39-高亮范围修复';
+  SR.VERSION = 'v40-去元叙述';
   console.log('%c[SR] 苏格拉底阅读器 ' + SR.VERSION, 'color:#e3b34c;font-weight:bold');
   /* PDF.js：本地 vendor 优先，CDN 兜底 */
   try {
